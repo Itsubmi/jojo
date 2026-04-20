dd4d167b3d4e49ddb00c9aba594f3f11810734ac
Tseren-Ubushieva Izhilina <itsubmiloopse@gmail.com>
2026-04-20 14:18:48 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+BOT_TOKEN = "8737916757:AAFRpNsNhfduySpbHOwOEwr7nAvOJlvPCAg"
+
+# Настройки Gigachat
+GIGACHAT_CREDENTIALS = "MDE5ZDZmMWEtYTJlOS03ZjgxLWI2ODYtMzRlYjg1MjBlMmQ0OmJjM2ZiYmNhLTk1Y2YtNDhjMi05Y2Q3LWI4ZTlhNjk0MzhlMw=="  # API ключ от Gigachat
+GIGACHAT_SCOPE = "GIGACHAT_API_PERS"  # или другой scope
+
+# Путь к Excel файлу с товарами
+PRODUCTS_EXCEL_PATH = "products.xlsx"
